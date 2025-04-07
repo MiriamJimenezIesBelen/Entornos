98af6d6feb0384348342e498e8cc0b1a4fee23a5
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-04-07 13:22:22 +0200

    2 commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
   public static void main(String[] args) {
     System.out.println("Estamos haciendo control de versiones con Git");
+    
+    System.out.println("Ya hemos hecho una vez el commit");
 
   }
 
